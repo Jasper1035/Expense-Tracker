fc53e39c794ec4cdbda43d1b3083f4fe81d07448
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-07-11 18:46:52 +0100

    project set up

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:expense_tracker/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
