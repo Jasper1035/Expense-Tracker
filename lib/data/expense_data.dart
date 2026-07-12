f6a5281bf10b33e0712515870770fb3a78c79307
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-07-12 06:40:10 +0100

    add the days

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -14,4 +14,37 @@ class ExpenseData {
   void deleteExpnese(ExpenseItem expense) {
     overallExpenseList.remove(expense);
   }
+
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thur';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return 'Mon';
+    }
+  }
+
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+    DateTime today = DateTime.now();
+
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Mon') {
+        startOfWeek = today.subtract(Duration(days: i));
+      }
+    }
+    return startOfWeek!;
+  }
 }
